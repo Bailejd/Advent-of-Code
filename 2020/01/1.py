8422c7e6996dc75fa95ef12931c0afd1b957a8d4
Bailejd <35389557+Bailejd@users.noreply.github.com>
2021-09-26 21:40:10 -0400

    Update 1.py

--- 2020/01/1.py
+++ 2020/01/1.py
@@ -1,10 +1,13 @@
-# Day 1
+# Day 1: Report Repair
+
+import pathlib
+
+DIR = pathlib.Path(__file__).parent.absolute()
 
 def part_one(data_list):
     for i in range(len(data_list)):
         for j in range(len(data_list)):
             if(i != j and data_list[i] + data_list[j] == 2020):
-                print(f'{data_list[i]} + {data_list[j]} = 2020')
                 return(data_list[i] * data_list[j])
 
 
@@ -13,13 +16,12 @@ def part_two(data_list):
         for j in range(len(data_list)):
             for k in range(len(data_list)):
                 if(i != j and j != k and data_list[i] + data_list[j] + data_list[k] == 2020):
-                    print(f'{data_list[i]} + {data_list[j]} + {data_list[k]} = 2020')
                     return(data_list[i] * data_list[j] * data_list[k])
 
 
 def main():
     # Read input file
-    with open("./input.txt", "r") as f:
+    with open(DIR / "input.txt", "r") as f:
         data_list = f.read()
 
     data_list = data_list.split('\n')
