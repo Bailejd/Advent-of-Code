1667c5df9363eb4b2634a6a6cad863b8833f82db
Bailejd <35389557+Bailejd@users.noreply.github.com>
2021-12-04 02:31:23 -0500

    Complete day 2

---
+++ 2021/02/2.py
@@ -0,0 +1,55 @@
+# Day 2: Dive!
+
+def part_one(data_list):
+    horizonal = 0
+    depth = 0
+
+    for direction in data_list:
+        dir, amount = direction.split()
+
+        if(dir == 'forward'):
+            horizonal += int(amount)
+        elif(dir == 'down'):
+            depth += int(amount)
+        elif(dir == 'up'):
+            depth -= int(amount)
+
+    return horizonal * depth
+
+
+def part_two(data_list):
+    horizonal = 0
+    depth = 0
+    aim = 0
+
+    for direction in data_list:
+        dir, amount = direction.split()
+        amount = int(amount)
+
+        if(dir == 'forward'):
+            horizonal += amount
+            depth += aim * amount
+        elif(dir == 'down'):
+            aim += amount
+        elif(dir == 'up'):
+            aim -= amount
+
+    return horizonal * depth
+
+
+def main():
+    # Read input file
+    with open("./input.txt", "r") as f:
+        data_list = f.read()
+    
+    data_list = data_list.split('\n')
+
+    # Part 1
+    print(f'Part 1: {part_one(data_list)}')
+
+    # Part 2
+    print(f'Part 2: {part_two(data_list)}')
+  
+    
+if __name__ == '__main__':
+    main()
