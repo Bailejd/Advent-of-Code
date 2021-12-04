5be9c6ecabdadee7282f3bc8da2747bf922f04e5
Bailejd <35389557+Bailejd@users.noreply.github.com>
2021-12-04 02:31:09 -0500

    Add 2021 directory and complete day 1

---
+++ 2021/01/1.py
@@ -0,0 +1,43 @@
+# Day 1: Sonar Sweep
+
+def part_one(data_list):
+    incr = 0
+
+    for prev_, next_ in zip(data_list, data_list[1:]):
+        if(prev_ < next_):
+            incr += 1
+    
+    return incr
+    
+
+def part_two(data_list):
+    incr = 0
+    sums = []
+
+    for prev_, curr_, next_ in zip(data_list, data_list[1:], data_list[2:]):
+        sums.append(prev_ + curr_ + next_)
+
+    for prev_, next_ in zip(sums, sums[1:]):
+        if(prev_ < next_):
+            incr += 1
+    
+    return incr
+
+
+def main():
+    # Read input file
+    with open("./input.txt", "r") as f:
+        data_list = f.read()
+    
+    data_list = data_list.split('\n')
+    data_list = [int(i) for i in data_list]
+
+    # Part 1
+    print(f'Part 1: {part_one(data_list)}')
+
+    # Part 2
+    print(f'Part 2: {part_two(data_list)}')
+  
+    
+if __name__ == '__main__':
+    main()
