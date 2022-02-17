d4d50ccdcaf7318d3a3911004ab60f9e6158510d
Bailejd <35389557+Bailejd@users.noreply.github.com>
2022-02-16 22:08:52 -0500

    Remove unused code

--- 2015/03/day3.java
+++ 2015/03/day3.java
@@ -8,12 +8,10 @@ public class day3 {
     public static class Coords {
         private int x;
         private int y;
-        private int hashCode;
 
         public Coords(int x, int y) {
             this.x = x;
             this.y = y;
-            this.hashCode = Objects.hash(x, y);
         }
 
         public int getX() {
@@ -29,22 +27,6 @@ public class day3 {
             else if(direction == 'v') this.y -= 1;
             else if(direction == '>') this.x += 1;
             else if(direction == '<') this.x -= 1;
-
-            this.hashCode = Objects.hash(x, y);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if(this == o) return true;
-            if(o == null || getClass() != o.getClass()) return false;
-
-            Coords that = (Coords) o;
-            return x == that.x && y == that.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return this.hashCode;
         }
 
         @Override
