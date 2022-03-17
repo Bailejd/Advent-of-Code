cd10cddc29ba7f77fd82e05c8944ab3713296d83
Bailejd <35389557+Bailejd@users.noreply.github.com>
2022-03-17 01:38:56 -0400

    Complete day 10

---
+++ 2015/10/day10.java
@@ -0,0 +1,57 @@
+// Day 10: Elves Look, Elves Say
+//
+// Results
+// Part 1: 492982
+// Part 2: 6989950
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class day10 {
+    
+    public static void main(String[] args) throws Exception {
+        String data = readInput();
+
+        // Part 1
+        System.out.println(lookAndSay(data, 40));
+
+        // Part 2
+        System.out.println(lookAndSay(data, 50));
+    }
+
+    public static int lookAndSay(String data, int steps) {
+        String input = data;
+
+        for(int i = 0; i < steps; i++) {
+            StringBuilder new_string = new StringBuilder();
+
+            for(int j = 0; j < input.length(); j++) {
+                int count = 1;
+                char ch = input.charAt(j);
+
+                while (j + 1 < input.length() && input.charAt(j + 1) == ch) {
+                    j++;
+                    count++;
+                }
+
+                new_string.append(count);
+                new_string.append(ch);
+            }
+
+            input = new_string.toString();
+        }
+
+        return input.length();
+    }
+
+    public static String readInput() throws Exception {
+        String data = "";
+        data = new String(Files.readAllBytes(Paths.get("./input.txt")));
+        return data; 
+    }
+}
